5e4712a2a2ad3a50d9f39d1d13d6969ee89c3860
PetxCode <brighterdayscodelab@gmail.com>
2022-05-11 18:11:10 +0100

    completed

--- model/userModel.js
+++ model/userModel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const userModel = mongoose.Schema(
 	{
-		count: {
+		userName: {
 			type: String,
 		},
 		email: {
--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
-const url = "mongodb://localhost/marketPlace";
+const url = "mongodb://localhost/marketPlaceDB";
 
 const uri =
 	"mongodb+srv://comsol:comsol@cluster0.kjlbe.mongodb.net/marketPlace?retryWrites=true&w=majority";
 
-mongoose.connect(uri).then(() => {
+mongoose.connect(url).then(() => {
 	console.log("Database now connected...!");
 });
